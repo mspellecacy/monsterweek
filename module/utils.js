0ea9bf0044678d70d1245a994420c7d0b3cce60d
Michael <mspellecacy@frakle.com>
2021-12-16 10:14:48 -0500

    - Branch management

--- module/utils.js
+++ module/utils.js
@@ -1,14 +1,22 @@
+import { SimpleActorSheet } from "./actor-sheet.js";
+
 export class Utils {
 
     static moveDescriptionToggler(elem, actor, itemId) {
-        const item = actor.items.get(itemId);
+        let item = actor.items.get(itemId);
+
+        let desc = item?.data.data.description || "";
+
+        if(!item) {
+            item = {data: {data: {description: 'err'}}};
+        }
 
         // Toggle summary
         if (elem.hasClass("item-expanded")) {
             let summary = elem.children(".item-summary");
             summary.slideUp(200, () => summary.remove());
         } else {
-            const div = $(`<div class="item-summary">${item.data.data.description}</div>`);
+            const div = $(`<div class="item-summary">${desc}</div>`);
             elem.append(div.hide());
             div.slideDown(200);
         }
